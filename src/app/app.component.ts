6bb73ecdb36e49728fdf1850cab8856289ea0edb
dhapper <dhaarshan.p@gmail.com>
2025-01-13 18:59:24 -0500

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './components/header/header.component';
+import { BodyComponent } from "./components/body/body.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [
+    HeaderComponent,
+    BodyComponent
+],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
